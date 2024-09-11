aba12d9e88cc9883199778823143f689a5fb6d36
hameem2003 <hameem007mustafa@gmail.com>
2024-09-11 22:45:52 +0600

    CRUD With DataGridView

---
+++ CRUD with DataGridView/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace CRUD_with_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_address = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_phonenumber = new System.Windows.Forms.TextBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_update = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_show_alldata = new System.Windows.Forms.Button();
+            this.textBox_search_id = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 26);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(653, 271);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(96, 328);
+            this.textBox_id.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_id.Multiline = true;
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(134, 28);
+            this.textBox_id.TabIndex = 1;
+            this.textBox_id.Enter += new System.EventHandler(this.textBox_id_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 331);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_address
+            // 
+            this.textBox_address.Location = new System.Drawing.Point(96, 392);
+            this.textBox_address.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_address.Multiline = true;
+            this.textBox_address.Name = "textBox_address";
+            this.textBox_address.Size = new System.Drawing.Size(134, 28);
+            this.textBox_address.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 407);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Address";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(262, 331);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Name";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(345, 328);
+            this.textBox_name.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_name.Multiline = true;
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(134, 28);
+            this.textBox_name.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(262, 407);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "PhoneNumber";
+            // 
+            // textBox_phonenumber
+            // 
+            this.textBox_phonenumber.Location = new System.Drawing.Point(345, 392);
+            this.textBox_phonenumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_phonenumber.Multiline = true;
+            this.textBox_phonenumber.Name = "textBox_phonenumber";
+            this.textBox_phonenumber.Size = new System.Drawing.Size(134, 28);
+            this.textBox_phonenumber.TabIndex = 9;
+            this.textBox_phonenumber.Enter += new System.EventHandler(this.textBox_phonenumber_Enter);
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(550, 325);
+            this.button_add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(83, 31);
+            this.button_add.TabIndex = 13;
+            this.button_add.Text = "ADD";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(670, 322);
+            this.button_update.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(83, 31);
+            this.button_update.TabIndex = 14;
+            this.button_update.Text = "UPDATE";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(550, 386);
+            this.button_delete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(83, 31);
+            this.button_delete.TabIndex = 15;
+            this.button_delete.Text = "DELETE";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // button_show_alldata
+            // 
+            this.button_show_alldata.Location = new System.Drawing.Point(670, 386);
+            this.button_show_alldata.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_show_alldata.Name = "button_show_alldata";
+            this.button_show_alldata.Size = new System.Drawing.Size(83, 31);
+            this.button_show_alldata.TabIndex = 16;
+            this.button_show_alldata.Text = "ShowAllData";
+            this.button_show_alldata.UseVisualStyleBackColor = true;
+            this.button_show_alldata.Click += new System.EventHandler(this.button_show_alldata_Click);
+            // 
+            // textBox_search_id
+            // 
+            this.textBox_search_id.Location = new System.Drawing.Point(691, 37);
+            this.textBox_search_id.Name = "textBox_search_id";
+            this.textBox_search_id.Size = new System.Drawing.Size(100, 20);
+            this.textBox_search_id.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(691, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Search ID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 457);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_search_id);
+            this.Controls.Add(this.button_show_alldata);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.textBox_address);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_phonenumber);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_address;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_phonenumber;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_show_alldata;
+        private System.Windows.Forms.TextBox textBox_search_id;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CRUD with DataGridView/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+using System.Data.SqlClient;
+
+namespace CRUD_with_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'cRUD_with_DataGridViewDatabaseDataSet.Table' table. You can move, or remove it, as needed.
+            //this.tableTableAdapter.Fill(this.cRUD_with_DataGridViewDatabaseDataSet.Table);
+
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            // Check if any of the text boxes are empty
+            if (string.IsNullOrWhiteSpace(textBox_id.Text) ||
+                string.IsNullOrWhiteSpace(textBox_name.Text) ||
+                string.IsNullOrWhiteSpace(textBox_phonenumber.Text) ||
+                string.IsNullOrWhiteSpace(textBox_address.Text))
+            {
+                MessageBox.Show("All fields must be filled.");
+                return; // Exit the method if validation fails
+            }
+
+            using (SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\project202\\CRUD with DataGridView\\Database\\CRUD with DataGridViewDatabase.mdf\";Integrated Security=True"))
+            {
+                try
+                {
+                    con.Open();
+
+                    // Check if the ID is already registered
+                    SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM [Table] WHERE id = @id", con);
+                    checkCmd.Parameters.AddWithValue("@id", textBox_id.Text);
+                    int idExists = (int)checkCmd.ExecuteScalar();
+
+                    if (idExists > 0)
+                    {
+                        MessageBox.Show("This ID is already registered.");
+                        return; // Exit the method if ID already exists
+                    }
+
+                    // If ID is not already registered, insert the new record
+                    SqlCommand cmd = new SqlCommand("INSERT INTO [Table] (id, name, phonenumber, address) VALUES (@id, @name, @phonenumber, @address)", con);
+                    cmd.Parameters.AddWithValue("@id", textBox_id.Text);
+                    cmd.Parameters.AddWithValue("@name", textBox_name.Text);
+                    cmd.Parameters.AddWithValue("@phonenumber", textBox_phonenumber.Text);
+                    cmd.Parameters.AddWithValue("@address", textBox_address.Text);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Successfully Saved");
+
+                    // Now retrieve all data from the table to show in the DataGridView
+                    SqlCommand selectCmd = new SqlCommand("SELECT * FROM [Table]", con);
+                    SqlDataAdapter da = new SqlDataAdapter(selectCmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Bind the DataTable to the DataGridView
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            // Check if the user has entered an ID
+            if (string.IsNullOrWhiteSpace(textBox_id.Text))
+            {
+                MessageBox.Show("ID is mandatory for updating. Please fill in the ID.");
+                return; // Exit the method to prevent further execution
+            }
+
+            using (SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\project202\\CRUD with DataGridView\\Database\\CRUD with DataGridViewDatabase.mdf\";Integrated Security=True"))
+            {
+                try
+                {
+                    con.Open();
+
+                    // Check if the ID exists before updating
+                    SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM [Table] WHERE id = @id", con);
+                    checkCmd.Parameters.AddWithValue("@id", textBox_id.Text);
+                    int idExists = (int)checkCmd.ExecuteScalar();
+
+                    if (idExists == 0)
+                    {
+                        MessageBox.Show("ID does not exist. Please provide a valid ID.");
+                        return; // Exit the method if the ID does not exist
+                    }
+
+                    // Update the record without changing the ID
+                    SqlCommand cmd = new SqlCommand("UPDATE [Table] SET name = @name, phonenumber = @phonenumber, address = @address WHERE id = @id", con);
+                    cmd.Parameters.AddWithValue("@id", textBox_id.Text); // ID remains unchanged
+                    cmd.Parameters.AddWithValue("@name", textBox_name.Text);
+                    cmd.Parameters.AddWithValue("@phonenumber", textBox_phonenumber.Text);
+                    cmd.Parameters.AddWithValue("@address", textBox_address.Text);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Successfully Updated");
+
+                    // Refresh the DataGridView with updated data
+                    SqlCommand selectCmd = new SqlCommand("SELECT * FROM [Table]", con);
+                    SqlDataAdapter da = new SqlDataAdapter(selectCmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Bind the DataTable to the DataGridView
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            // Check if the user has entered an ID
+            if (string.IsNullOrWhiteSpace(textBox_id.Text))
+            {
+                MessageBox.Show("ID is mandatory for deleting. Please fill in the ID.");
+                return; // Exit the method to prevent further execution
+            }
+
+            using (SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\project202\\CRUD with DataGridView\\Database\\CRUD with DataGridViewDatabase.mdf\";Integrated Security=True"))
+            {
+                try
+                {
+                    con.Open();
+
+                    // Check if the ID exists before attempting to delete
+                    SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM [Table] WHERE id = @id", con);
+                    checkCmd.Parameters.AddWithValue("@id", textBox_id.Text);
+                    int idExists = (int)checkCmd.ExecuteScalar();
+
+                    if (idExists == 0)
+                    {
+                        MessageBox.Show("No data found for the given ID.");
+                        return; // Exit the method if no matching ID is found
+                    }
+
+                    // If the ID exists, proceed with the deletion
+                    SqlCommand cmd = new SqlCommand("DELETE FROM [Table] WHERE id = @id", con);
+                    cmd.Parameters.AddWithValue("@id", textBox_id.Text);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Successfully Deleted");
+
+                    // Now retrieve the updated data from the table to show in the DataGridView
+                    SqlCommand selectCmd = new SqlCommand("SELECT * FROM [Table]", con);
+                    SqlDataAdapter da = new SqlDataAdapter(selectCmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Bind the DataTable to the DataGridView to reflect the updated data
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check if the user entered a value in the search TextBox
+            if (string.IsNullOrWhiteSpace(textBox_search_id.Text))
+            {
+                MessageBox.Show("Please enter a valid ID.");
+                return; // Exit the method to prevent further execution
+            }
+
+            using (SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\project202\\CRUD with DataGridView\\Database\\CRUD with DataGridViewDatabase.mdf\";Integrated Security=True"))
+            {
+                try
+                {
+                    con.Open();
+
+                    // Prepare the SQL query to search for the specified ID
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM [Table] WHERE id = @id", con);
+                    cmd.Parameters.AddWithValue("@id", textBox_search_id.Text);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Check if any rows were returned
+                    if (dt.Rows.Count > 0)
+                    {
+                        // Bind the DataTable to the DataGridView to show the results
+                        dataGridView1.DataSource = dt;
+                    }
+                    else
+                    {
+                        // Show a message if no matching ID is found
+                        MessageBox.Show("No registered ID is available.");
+                        dataGridView1.DataSource = null; // Clear the DataGridView if nothing is found
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+        private void button_show_alldata_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\project202\\CRUD with DataGridView\\Database\\CRUD with DataGridViewDatabase.mdf\";Integrated Security=True"))
+            {
+                try
+                {
+                    con.Open();
+
+                    // Query to select all data from the table
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM [Table]", con);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Check if any rows were returned
+                    if (dt.Rows.Count > 0)
+                    {
+                        // Bind the DataTable to the DataGridView to show the results
+                        dataGridView1.DataSource = dt;
+                    }
+                    else
+                    {
+                        // If no data is found, clear the DataGridView
+                        dataGridView1.DataSource = null;
+                        MessageBox.Show("No data found.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+        private void textBox_id_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_phonenumber_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
